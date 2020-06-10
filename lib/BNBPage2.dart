a5962dc838a6bdd4692808e44fe3b3b2f1390d36
yushilong <yushilong@shihuo.cn>
2020-06-10 22:32:08 +0800

    1

--- lib/BNBPage2.dart
+++ lib/BNBPage2.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scrolling_page_indicator/scrolling_page_indicator.dart';
 
 class BNBPage2 extends StatelessWidget {
+  PageController _pageController = PageController();
+  List<Widget> pages = [
+    Container(
+      color: Colors.teal,
+      child: Text("page1"),
+    ),
+    Container(
+      color: Colors.amberAccent,
+      child: Text("page2"),
+    ),
+    Container(
+      color: Colors.blue,
+      child: Text("page3"),
+    ),
+    Container(
+      color: Colors.brown,
+      child: Text("page4"),
+    ),
+    Container(
+      color: Colors.deepPurpleAccent,
+      child: Text("page5"),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -15,8 +40,32 @@ class BNBPage2 extends StatelessWidget {
           },
         ),
       ),
-      body: Center(
-        child: Text("page2"),
+      body: SizedBox(
+        height: 300,
+        child: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: <Widget>[
+            Expanded(
+              child: PageView(
+                children: pages,
+                controller: _pageController,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+              child: ScrollingPageIndicator(
+                dotColor: Colors.grey,
+                dotSelectedColor: Colors.blue,
+                dotSize: 6,
+                dotSelectedSize: 8,
+                dotSpacing: 12,
+                controller: _pageController,
+                itemCount: pages.length,
+                orientation: Axis.horizontal,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
